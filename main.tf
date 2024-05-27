296cb9f8313f2fb46382aa985eb85f89b53b3157
acassera <acassera@paloaltonetworks.com>
2024-05-27 11:37:02 -0500

    update vms

--- main.tf
+++ main.tf
@@ -30,8 +30,8 @@ locals {
   virtual_machines = {
     "vm1" = { size = local.vm_size1, location = local.location1, resource_group = local.rg1, subnet_id = local.subnet1_id },
     "vm2" = { size = local.vm_size1, location = local.location1, resource_group = local.rg1, subnet_id = local.subnet1_id },
-    "vm3" = { size = local.vm_size1, location = local.location1, resource_group = local.rg1, subnet_id = local.subnet1_id }
-
+    "vm3" = { size = local.vm_size1, location = local.location1, resource_group = local.rg1, subnet_id = local.subnet1_id },
+    "vm4" = { size = local.vm_size1, location = local.location1, resource_group = local.rg1, subnet_id = local.subnet1_id }
   }
 }
 
